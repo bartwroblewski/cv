0070f8e2a67225df5fc04208b628716c67bd8d8b
bartwroblewski <barti.wroblewski@gmail.com>
2023-06-26 12:53:14 +0000

    ok

--- views.py
+++ views.py
@@ -6,6 +6,10 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/echo')
+def echo():
+    return 'hi'
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
